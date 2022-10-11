fe900d40007bd057a00d631eac6a46fc3d73ab3b
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2022-10-11 08:08:48 +0700

    Part 1 Landing Page and User Authentication - Setting up

--- django_insta_media_website/settings.py
+++ django_insta_media_website/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social',
+    'landing',
+
 ]
 
 MIDDLEWARE = [
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'django_insta_media_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_socialmedia',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
