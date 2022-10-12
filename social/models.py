f1012ae71a7a3c732a00dd05304f7878aab3cfcf
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2022-10-12 19:22:34 +0700

    Fix Slug in Social app- not finished yet

--- social/models.py
+++ social/models.py
@@ -1,3 +1,57 @@
+import random
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+from django.db.models.signals import pre_save, post_save
 
 # Create your models here.
+class Post(models.Model):
+    body = models.TextField()
+    created_on = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    slug = models.SlugField(blank=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        #self.slug = slugify(self.body)
+        super(Post, self).save()
+
+    def __str__(self):
+        return "{} . {} ".format(self.id, self.author)
+
+def slugify_instance_body(instance, save=False, new_slug=None):
+    if new_slug is not None:
+        slug = new_slug
+    else:
+        slug = slugify(instance.body)
+    Klass = instance.__class__
+    bd = Klass.objects.filter(slug=slug).exclude(id=instance.id)
+    if bd.exists():
+        # auto generate new slug
+        rand_int = random.randint(300_000, 500_000)
+        slug = f"{slug}-{rand_int}"
+        return slugify_instance_body(instance, save=save, new_slug=slug)
+    instance.slug = slug
+    if save:
+        instance.save()
+    return instance
+
+def post_pre_save(sender, instance, *args, **kwargs):
+    #print(sender, instance)
+    #if you uncomment bellow it means you slug field should be editable can add manually
+    if instance.slug is None:
+        # slug = slugify(instance.body)
+        # instance.slug = slug
+        slugify_instance_body(instance, save=False)
+
+
+pre_save.connect(post_pre_save, sender=Post)
+
+def post_post_save(sender, instance, created,*args, **kwargs):
+    if created:
+        # slug = slugify(instance.body)
+        # instance.slug = slug
+        # instance.save()
+        slugify_instance_body(instance, save=True)
+
+post_save.connect(post_post_save, sender=Post)
---
+++ social/utils.py
@@ -0,0 +1,4 @@
+import random
+from django.utils.text import slugify
+
+
