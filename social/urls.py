93c22998317075bc82eb839360c15abebda28cb1
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2022-10-15 09:59:10 +0700

    Building a Social Media App : Part 4 - OnProcess

--- social/urls.py
+++ social/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostEditView, PostDeleteView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
-    path('post-detail/<slug:slugInput>/', PostDetailView.as_view(), name='post-detail'),
+    path('post-detail/<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
+    path('post-edit/<slug:slug>/', PostEditView.as_view(), name='post-edit'),
+    path('post-delete/<slug:slug>/', PostDeleteView.as_view(), name='post-delete'),
 ]
+
+#humans-are-idiot
--- social/views.py
+++ social/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
-from .models import Post
+from .models import Post, Comment
 from .forms import PostForm, CommentForm
+from django.views.generic.edit import UpdateView, DeleteView
 # Create your views here.
 
 class PostListView(View):
@@ -33,12 +35,47 @@ class PostListView(View):
 
 class PostDetailView(View):
 
-    def get(self, request, slugInput, *args, **kwargs):
-        post = Post.objects.get(slug=slugInput)
+    def get(self, request, slug, *args, **kwargs):
+        post = Post.objects.get(slug=slug)
         form = CommentForm()
+        comments = Comment.objects.filter(post=post).order_by('-created_on')
+
         context = {
             'post' : post,
             'form': form,
+            'comments' : comments,
         }
 
         return render(request, 'social/post_detail.html', context)
+
+    def post(self, request, slug, *args, **kwargs):
+        post = Post.objects.get(slug=slug)
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.author = request.user
+            new_comment.post = post
+            new_comment.save()
+
+        comments = Comment.objects.filter(post=post).order_by('-created_on')
+        context = {
+            'post': post,
+            'form': form,
+            'comments': comments,
+        }
+        return render(request, 'social/post_detail.html', context)
+class PostEditView(UpdateView):
+    model = Post
+    fields = ['body']
+    template_name = 'social/post_edit.html'
+    print("hello masuk")
+    def get_success_url(self):
+
+        slug = self.kwargs['slug']
+        return  reverse_lazy('post-detail', kwargs={'slug': slug})
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'social/post_delete.html'
+    success_url = reverse_lazy('post-list')
