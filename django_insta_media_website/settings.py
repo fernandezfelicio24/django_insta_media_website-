ee95e412b2e5a498252cd65042ad97823c644db6
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2022-10-18 15:14:57 +0700

    Still in Part 5 - Debug setting django image display

--- django_insta_media_website/settings.py
+++ django_insta_media_website/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -141,9 +141,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
+
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
--- social/urls.py
+++ social/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView
 
 urlpatterns = [
+
     path('', PostListView.as_view(), name='post-list'),
     path('post-detail/<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
     path('post-edit/<slug:slug>/', PostEditView.as_view(), name='post-edit'),
     path('post-delete/<slug:slug>/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<slug:slug>/comment/delete/<int:pk>/ ', CommentDeleteView.as_view(), name='comment-delete'),
+    path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
 ]
 
