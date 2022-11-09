77e52eb5a1e3dd2786d1cf3c97c1d05ddb3826b8
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2022-11-09 15:28:33 +0700

    Building Social Media App - Part 12

--- social/admin.py
+++ social/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 
 # Register your models here.
-from .models import Post, UserProfile, Comment
+from .models import Post, UserProfile, Comment, Notification
 
 class PostAdmin(admin.ModelAdmin):
 
@@ -11,3 +11,4 @@ class PostAdmin(admin.ModelAdmin):
 admin.site.register(Post, PostAdmin)
 admin.site.register(UserProfile)
 admin.site.register(Comment)
+admin.site.register(Notification)
